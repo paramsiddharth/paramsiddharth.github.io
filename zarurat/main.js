36db9f850dbd5b4a2c102634ce79bf2784bf34bb
Param Siddharth <contact@paramsid.com>
2021-02-18 17:56:00 +0530

    Use Tabs for indentation

--- zarurat/main.js
+++ zarurat/main.js
@@ -5,98 +5,98 @@ mob = false,
 desk = false;
 
 $(function() {
-    sizeHandler();
+	sizeHandler();
 
 });
 
 const sizeHandler = function() {
-    if ($(window).width() < 992) {
-        $('#top').show();
-        $('#main').css({
-            'padding-top': '+=' + $('#top').outerHeight()//+$('#menu').outerHeight()
-        });
-        $('#bottom').show();
-        $('.desktop').hide();
-        if (!mob) mobileHandler();
-    } else {
-        $('#top').show();
-        $('.top-menu').hide();
-        $('#menu').show();
-        $('#main').css({
-            'padding-top': '+=' + $('#top').outerHeight()//+$('#menu').outerHeight()
-        });
-        $('.mobile').hide();
-        $('#bottom').show();
-    }
+	if ($(window).width() < 992) {
+		$('#top').show();
+		$('#main').css({
+			'padding-top': '+=' + $('#top').outerHeight()//+$('#menu').outerHeight()
+		});
+		$('#bottom').show();
+		$('.desktop').hide();
+		if (!mob) mobileHandler();
+	} else {
+		$('#top').show();
+		$('.top-menu').hide();
+		$('#menu').show();
+		$('#main').css({
+			'padding-top': '+=' + $('#top').outerHeight()//+$('#menu').outerHeight()
+		});
+		$('.mobile').hide();
+		$('#bottom').show();
+	}
 };
 
 const mobileHandler = function() {
-    if (!mob) mob = true;
-    $(".top-menu").on('click', function() {
-        if (!menuToggled) {
-            $(this).animate({
-                rotationDeg: 90
-            },
-                {
-                    duration: toggleTime,
-                    step: function(angle) {
-                        $(this).css({
-                            transform: 'rotate(' + angle + 'deg)'
-                        });
-                    }
-                }
-            );
-            setTimeout(function() {
-                $('#menu').slideDown(toggleTime);
-            }, delayTime);
-            menuToggled = true;
-            $(document).on('mouseup.outSideClick', function(p) {
-                let elem = $('#menu');
-                if (
-                    menuToggled &&
-                    //!$('.top-menu').is(p.target) &&
-                    //$('.top-menu').has(p.target).length == 0 &&
-                    $(p.target).closest('.top-menu').length == 0 &&
-                    !elem.is(p.target) &&
-                    elem.has(p.target).length == 0
-                ) {
-                    $('.top-menu').animate({
-                        rotationDeg: 0
-                    },
-                        {
-                            duration: toggleTime,
-                            step: function(angle) {
-                                $(this).css({
-                                    transform: 'rotate(' + angle + 'deg)'
-                                });
-                            }
-                        }
-                    );
-                    setTimeout(function() {
-                        $('#menu').slideUp(toggleTime);
-                    }, delayTime);
-                    menuToggled = false;
-                    $(document).off('.outSideClick');
-                }
-            })
-        } else {
-            $(this).animate({
-                rotationDeg: 0
-            },
-                {
-                    duration: toggleTime,
-                    step: function(angle) {
-                        $(this).css({
-                            transform: 'rotate(' + angle + 'deg)'
-                        });
-                    }
-                }
-            );
-            setTimeout(function() {
-                $('#menu').slideUp(toggleTime);
-            }, delayTime);
-            menuToggled = false;
-            $(document).off('.outSideClick');
-        }
-    });
+	if (!mob) mob = true;
+	$(".top-menu").on('click', function() {
+		if (!menuToggled) {
+			$(this).animate({
+				rotationDeg: 90
+			},
+				{
+					duration: toggleTime,
+					step: function(angle) {
+						$(this).css({
+							transform: 'rotate(' + angle + 'deg)'
+						});
+					}
+				}
+			);
+			setTimeout(function() {
+				$('#menu').slideDown(toggleTime);
+			}, delayTime);
+			menuToggled = true;
+			$(document).on('mouseup.outSideClick', function(p) {
+				let elem = $('#menu');
+				if (
+					menuToggled &&
+					//!$('.top-menu').is(p.target) &&
+					//$('.top-menu').has(p.target).length == 0 &&
+					$(p.target).closest('.top-menu').length == 0 &&
+					!elem.is(p.target) &&
+					elem.has(p.target).length == 0
+				) {
+					$('.top-menu').animate({
+						rotationDeg: 0
+					},
+						{
+							duration: toggleTime,
+							step: function(angle) {
+								$(this).css({
+									transform: 'rotate(' + angle + 'deg)'
+								});
+							}
+						}
+					);
+					setTimeout(function() {
+						$('#menu').slideUp(toggleTime);
+					}, delayTime);
+					menuToggled = false;
+					$(document).off('.outSideClick');
+				}
+			})
+		} else {
+			$(this).animate({
+				rotationDeg: 0
+			},
+				{
+					duration: toggleTime,
+					step: function(angle) {
+						$(this).css({
+							transform: 'rotate(' + angle + 'deg)'
+						});
+					}
+				}
+			);
+			setTimeout(function() {
+				$('#menu').slideUp(toggleTime);
+			}, delayTime);
+			menuToggled = false;
+			$(document).off('.outSideClick');
+		}
+	});
 };
--- zarurat/scripts/getsong.js
+++ zarurat/scripts/getsong.js
@@ -1,63 +1,63 @@
 $(function() {
-    let param = new URLSearchParams(location.search),
-    song = param.get('song'),
-    title = $('.songtitle'),
-    dat = $('.songdata'),
-    songdir = './songinfo/',
-    artdir = songdir+'art/';
-    $.getJSON(songdir+'song-'+song+'.json', function(obj) {
-        document.title = obj.name+' - Param Siddharth';
-        title.text(obj.name);
-        let t;
-        t = $('<p></p>').html(obj.about.replace(/\n/g, '<br/>'));
-        dat.append(t);
-        t = $('<img alt="Cover Art" style="display:block;width:80%;max-width:250px;margin:0 auto;border: solid 1px white;">').attr('src', artdir+obj.art);
-        dat.append(t);
-        t = $('<h2></h2>').text('Details');
-        dat.append(t);
-        t = $('<p></p>');
-        dat.append(t);
-        let list = $('<ul></ul>');
-        t.append(list);
-        for (let prop in obj.details) {
-            if (prop != 'Album') {
-                t = $('<li></li>').text(
-                    prop+': '+obj.details[prop]
-                );
-                list.append(t);
-            } else {
-                let det = obj.details[prop];
-                t = $('<a></a>').text(det[0]).attr({
-                    href: "./album.html?album="+det[1]
-                });
-                t = $('<li></li>').text(prop+': ').append(t);
-                list.append(t);
-                t = $('<a></a>').text(det[0]).attr({
-                    href: "./album.html?album="+det[1]
-                });
-                $('.breadcrumb .active').before(t);
-                t.after(' ');
-            }
-        }
-        t = $('<h2></h2>').text('Listen');
-        dat.append(t);
-        list = $('<ul></ul>');
-        dat.append(list);
-        for (let prop in obj.listen) {
-            t = $('<a target="_blank"></a>').text(prop).attr({
-                alt: prop,
-                href: obj.listen[prop]
-            });
-            list.append($('<li></li>').append(t));
-        }
-        t = $('<a target="_blank"></a>').text("More...").attr({
-            alt: "More...",
-            href: "http://google.com/search?q="+(obj.name+' by Param Siddharth').replace(/ /g, "%20")
-        });
-        list.append(t);
-        t = $('<h2></h2>').text('Lyrics');
-        dat.append(t);
-        t = $('<p></p>').html(obj.lyrics.replace(/\n/g, '<br/>'));
-        dat.append(t);
-    });
+	let param = new URLSearchParams(location.search),
+	song = param.get('song'),
+	title = $('.songtitle'),
+	dat = $('.songdata'),
+	songdir = './songinfo/',
+	artdir = songdir+'art/';
+	$.getJSON(songdir+'song-'+song+'.json', function(obj) {
+		document.title = obj.name+' - Param Siddharth';
+		title.text(obj.name);
+		let t;
+		t = $('<p></p>').html(obj.about.replace(/\n/g, '<br/>'));
+		dat.append(t);
+		t = $('<img alt="Cover Art" style="display:block;width:80%;max-width:250px;margin:0 auto;border: solid 1px white;">').attr('src', artdir+obj.art);
+		dat.append(t);
+		t = $('<h2></h2>').text('Details');
+		dat.append(t);
+		t = $('<p></p>');
+		dat.append(t);
+		let list = $('<ul></ul>');
+		t.append(list);
+		for (let prop in obj.details) {
+			if (prop != 'Album') {
+				t = $('<li></li>').text(
+					prop+': '+obj.details[prop]
+				);
+				list.append(t);
+			} else {
+				let det = obj.details[prop];
+				t = $('<a></a>').text(det[0]).attr({
+					href: "./album.html?album="+det[1]
+				});
+				t = $('<li></li>').text(prop+': ').append(t);
+				list.append(t);
+				t = $('<a></a>').text(det[0]).attr({
+					href: "./album.html?album="+det[1]
+				});
+				$('.breadcrumb .active').before(t);
+				t.after(' ');
+			}
+		}
+		t = $('<h2></h2>').text('Listen');
+		dat.append(t);
+		list = $('<ul></ul>');
+		dat.append(list);
+		for (let prop in obj.listen) {
+			t = $('<a target="_blank"></a>').text(prop).attr({
+				alt: prop,
+				href: obj.listen[prop]
+			});
+			list.append($('<li></li>').append(t));
+		}
+		t = $('<a target="_blank"></a>').text("More...").attr({
+			alt: "More...",
+			href: "http://google.com/search?q="+(obj.name+' by Param Siddharth').replace(/ /g, "%20")
+		});
+		list.append(t);
+		t = $('<h2></h2>').text('Lyrics');
+		dat.append(t);
+		t = $('<p></p>').html(obj.lyrics.replace(/\n/g, '<br/>'));
+		dat.append(t);
+	});
 });
